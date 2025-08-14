6e4e6c587e6360dce92a1359537613896aa77cae
laldroid <138674452+laldroid@users.noreply.github.com>
2025-08-14 14:08:04 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,7 @@
 # Specify the AWS provider
 provider "aws" {
-  region = "ap-southeast-2"   # Change to your preferred region
+  region = "ap-southeast-2"  
+  profile = "default" # Change to your preferred region
 }
 
 # Create the ECR repository
@@ -14,3 +15,4 @@ resource "aws_ecr_repository" "my_repo" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.my_repo.repository_url
 }
+
